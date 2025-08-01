846705e76d601f8c198e0905320b6554cffe144e
Joshua Pace <joshuapace24@gmail.com>
2025-08-01 01:33:32 -0700

    added the battle spaces for figments. the ui elements were made to make custom maps and layouts easy, though not if they have more or less spaces

--- battleScene.gd
+++ battleScene.gd
@@ -13,13 +13,39 @@ enum battlePositions{innerLeft, outerLeft, outerRight, innerRight}
 var alliedBattlers:Array[genericBattler] = []
 var enemyBattlers:Array[genericBattler] = []
 
+const figmentPosition_enemy_inner_left:Transform2D = Transform2D(0,Vector2(-75,25)) #  	Transform2D(rotation: float, position: Vector2)
+const figmentPosition_enemy_inner_right:Transform2D = Transform2D(0,Vector2(75,25))
+const figmentPosition_enemy_outer_left:Transform2D = Transform2D(0,Vector2(-75,-75))
+const figmentPosition_enemy_outer_right:Transform2D = Transform2D(0,Vector2(75,-75))
+
+const figmentPosition_ally_inner_left:Transform2D = Transform2D(0,Vector2(-75,125))
+const figmentPosition_ally_inner_right:Transform2D = Transform2D(0,Vector2(75,125))
+const figmentPosition_ally_outer_left:Transform2D = Transform2D(0,Vector2(-75,225))
+const figmentPosition_ally_outer_right:Transform2D = Transform2D(0,Vector2(75,225))
+
+var figmentPositions:Array[Transform2D] = [
+	figmentPosition_enemy_inner_left,
+	figmentPosition_enemy_inner_right,
+	figmentPosition_enemy_outer_left,
+	figmentPosition_enemy_outer_right,
+	figmentPosition_ally_inner_left,
+	figmentPosition_ally_inner_right,
+	figmentPosition_ally_outer_left,
+	figmentPosition_ally_outer_right]
+
 func _ready():
+
 	if battleInput:
 		for fig in debugTeam:
 			var figmentResource = load(fig)
 			enemyTeam.append(figment.new(figmentResource, debugLevel))
+			
 	for child in get_children():
-		if child is genericBattler:
+		#battle ui child
+		if child is battle_bakcgroundUI:
+			child.loadBattleUI()
+		#battler child	
+		elif child is genericBattler:
 			if "Allied" in child.name:
 				alliedBattlers.append(child)
 			elif "Enemy" in child.name:
@@ -28,7 +54,10 @@ func _ready():
 	if alliedBattlers.size() != 2 or enemyBattlers.size() != 2:
 		OS.alert("wrong number of battlers in battlescene. ", "ALERT") 
 		
+	#add figments to battlers
 	enemyBattlers[0].add_child(enemyTeam[0])
 	enemyBattlers[1].add_child(enemyTeam[1])
 	alliedBattlers[0].add_child(PlayerData.figmentTeam[0])
 	alliedBattlers[1].add_child(PlayerData.figmentTeam[1])
+	
+	#place the figments in scene
---
+++ battle_backgroundUI.gd
@@ -0,0 +1,26 @@
+class_name battle_bakcgroundUI
+extends Node
+
+var battlePositionNodes:Array[Sprite2D] = []
+
+#TODO i should clean this up. We need a texture 2D, and this in multiple lines o
+var positionSpriteImage:Image = Image.load_from_file("res://artDump/BattleSquare.png")
+var parentBattleSystem
+
+var loopIterator:int = 0
+func loadBattleUI():
+	parentBattleSystem = get_parent()
+	
+	#remember range doesnt include the final number itself. range(1,9) gives you 1 through 8 inclusive
+	for i in range(1,9):
+		var sprite = Sprite2D.new()
+		add_child(sprite)
+		battlePositionNodes.append((sprite))	
+		
+		sprite.transform = parentBattleSystem.figmentPositions[loopIterator]
+		sprite.texture = ImageTexture.create_from_image(positionSpriteImage)
+		sprite.z_index = 1
+		
+		loopIterator = loopIterator + 1
+	
+	
